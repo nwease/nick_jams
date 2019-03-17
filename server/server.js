3e6b1d59dfd7d2cc338fece026d037c82c11e677
Nicholas Wease <skynet60@outlook.com>
2019-03-17 13:15:29 +0100

    Working on setting up authentication. I suck at figuring it out at the moment LOL.

--- server/server.js
+++ server/server.js
@@ -30,6 +30,24 @@ app.post('/api/users/register',(req, res)=>{
     })
 });
 
+app.post('/api/users/login', (req, res) => {
+    User.findOne({'email': req.body.email}, (err, user) => {
+        if (!user) return res.json({loginSuccess: false, message: 'Email not found'});
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({loginSuccess: false, message: 'Wrong password'});
+
+            user.generateToken((err, user) => {
+                console.log('generateToken',err,user);
+                if (err) return res.status(400).send(err);
+                res.cookie('n_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
+        })
+    })
+});
+
 const port = process.env.PORT || 3002;
 app.listen(port,()=>{
     console.log(`Server Running at ${port}`)
